06127e554db480c3211252c919796d2c40dac662
Captain-Harlock <iosifidise@gmail.com>
2015-05-01 11:51:20 +0300

    Create btscan.py

---
+++ bluetooth/btscan.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+import time
+from bluetooth import *
+
+alreadyFound = []
+
+def findDevs():
+    foundDevs = discover_devices(lookup_names=True)
+    
+    for (addr,name) in foundDevs:
+        if addr not in alreadyFound:
+            print '[+] Found Bluetooth Device ' + name
+            print '[+] MAC Address: ' + addr
+            alreadyFound.append(addr)
+            
+
+while True:
+    print 'Searching for Bluetooth devices:'
+    findDevs()
+    print 'Waiting for 5 seconds until next scan...'
+    time.sleep(5)
