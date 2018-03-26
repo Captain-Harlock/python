4983d7517a54b7049166772fb9603abbcbd793ba
E.Iosifidis <iosifidise@gmail.com>
2018-03-26 18:25:27 +0300

    Wake on Lan Tool
    A tool that can be deployed on a raspberry pi with cron to wol network devices :)

---
+++ wol.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+
+import socket
+import struct
+
+def wake_on_lan(macaddress):
+    """ Switches on remote computers using WOL. """
+
+    # Check macaddress format and try to compensate.
+    if len(macaddress) == 12:
+        pass
+    elif len(macaddress) == 12 + 5:
+        sep = macaddress[2]
+        macaddress = macaddress.replace(sep, '')
+    else:
+        raise ValueError('Incorrect MAC address format')
+
+    # Pad the synchronization stream.
+    data = ''.join(['FFFFFFFFFFFF', macaddress * 16])
+    send_data = ''
+
+    # Split up the hex values and pack.
+    for i in range(0, len(data), 2):
+        send_data = ''.join([send_data,
+                             struct.pack('B', int(data[i: i + 2], 16))])
+
+    # Broadcast it to the LAN.
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+    print 'Sending data to Broadcast of length %s bytes'%len(send_data)
+    sock.sendto(send_data, ('<broadcast>', 7))
+
+if __name__ == '__main__':
+    # Use macaddresses with any seperators.
+    wake_on_lan('80:EE:73:A9:24:FF')
